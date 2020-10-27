16b66be9ed2c934097a2b50ee34b34ee1c8ab81a
loveuloveme <avicenna201360@gmail.com>
2020-10-28 01:31:10 +0300

    ps

--- server/processes/ProcessesList/main.cpp
+++ server/processes/ProcessesList/main.cpp
@@ -18,17 +18,62 @@
 #include <libprocstat.h>
 #include <sys/user.h>
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
-int main(void) {
-    struct kinfo_proc kp[1000] = {};
-	size_t len = sizeof(kp);
-    int mib[] = {CTL_KERN, KERN_PROC, KERN_PROC_PROC};
-	sysctl(mib, 3, &kp, &len, NULL, 0);
-    cout << kp << endl;
-    for(int i = 0; i < 1000; i++){
-        cout << kp[i].ki_ppid << endl;
+class PsProcess{
+    public:
+    pid_t pid;
+    pid_t parentPid;
+    char* name;
+
+    PsProcess(pid_t pid_, pid_t parentPid_, char* name_){
+        pid = pid_;
+        parentPid = parentPid_;
+        name = name_;
+        //print();
     }
+
+    void print(){
+        printf("%d %d %s\n", pid, parentPid, name);
+    }
+};
+
+class Ps{
+    private:
+    map<pid_t, PsProcess> processes;
+
+    public:
+    int maxProcessCount = 1044;
+
+    Ps(){
+        struct kinfo_proc processInfo[maxProcessCount] = {};
+        size_t len = sizeof(processInfo);
+        int mib[] = {CTL_KERN, KERN_PROC, KERN_PROC_PROC};
+        sysctl(mib, 3, &processInfo, &len, NULL, 0);
+        cout << processInfo << endl;
+        for(int i = 0; i < maxProcessCount; i++){
+            if(processInfo[i].ki_pid != 0){
+                processes.insert(pair<pid_t, PsProcess>(processInfo[i].ki_pid, PsProcess(processInfo[i].ki_pid, processInfo[i].ki_ppid, processInfo[i].ki_comm)));
+            }
+        }
+    }
+
+    void print(){
+        printf("Processes: \n");
+        for (auto it = processes.begin(); it != processes.end(); it++){
+            std::cout << it->first  // string (key)
+                    << ':'
+                    << it->second   // string's value 
+                    << std::endl ;
+        }
+    }
+};
+
+int main(void) {
+    Ps psRun;
+    psRun.print();
     return -1;
 }
